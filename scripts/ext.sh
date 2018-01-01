e4dc59cb56f7010d151b7e5de5cd22a267af4778
FujiHaruka <fujiharuka@realglobe.jp>
2018-01-01 15:56:42 +0900

    Add ext script

--- scripts/ext.sh
+++ scripts/ext.sh
@@ -6,3 +6,6 @@ cd `dirname $0`/../ext
 
 git clone https://github.com/biblicalhumanities/greek-new-testament.git
 git clone https://github.com/openscriptures/strongs.git
+
+# ないので
+echo 'module.exports = strongsGreekDictionary' >> ext/strongs/greek/strongs-greek-dictionary.js
